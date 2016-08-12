20dc1993468af63f3704c6625f06832bc63c9db9
vaness <vaness@Vanessa>
2016-08-11 22:31:58 -0600

    cuarto

--- Guia2_POO2_VanessaUrrutia/src/main/java/com/sv/udb/recursos/Conexion.java
+++ Guia2_POO2_VanessaUrrutia/src/main/java/com/sv/udb/recursos/Conexion.java
@@ -1,4 +1,8 @@
-
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package com.sv.udb.recursos;
 
 import java.io.File;
@@ -12,47 +16,47 @@ import java.util.Properties;
 public class Conexion {
     Connection conn = null;
     private String url, user, password;
-//
-//    public Connection getConn()
-//    {
-//        try
-//        {
-//            if(this.getDatosConexion())
-//            {
-//                DriverManager.registerDriver(new com.mysql.jdbc.Driver());
-//                conn = DriverManager.getConnection(this.url, this.user, this.password);
-//            }
-//        }
-//        catch(SQLException ex)
-//        {
-//            System.err.println("Error: " + ex.getMessage());
-//        }
-//        return conn;
-//    }
-//    
-//    private boolean getDatosConexion()
-//    {
-//        try 
-//        {
-//            Properties prop = new Properties();
-//               String propFileName = URLDecoder.decode(this.getClass().getClassLoader().getResource("/config.properties").getPath(),"UTF-8");
-//            File file = new File(propFileName);
-//            FileInputStream fis = new FileInputStream(file.getAbsoluteFile());
-//            prop.load(fis);
-//            if (fis == null)
-//            {
-//                throw new FileNotFoundException("Propiedades no encontradas dentro de: '" + propFileName + "'");
-//            }
-//            //Obtener Valores de la Conexion
-//            this.url = prop.getProperty("url");
-//            this.user = prop.getProperty("user");
-//            this.password = prop.getProperty("password");
-//            return true;
-//        }
-//       catch(Exception ex)
-//       {
-//           System.out.println(ex.getMessage());
-//           return true;
-//       }
-//    }
+
+    public Connection getConn()
+    {
+        try
+        {
+            if(this.getDatosConexion())
+            {
+                DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+                conn = DriverManager.getConnection(this.url, this.user, this.password);
+            }
+        }
+        catch(SQLException ex)
+        {
+            System.err.println("Error: " + ex.getMessage());
+        }
+        return conn;
+    }
+    
+    private boolean getDatosConexion()
+    {
+        try 
+        {
+            Properties prop = new Properties();
+            String propFileName = URLDecoder.decode(this.getClass().getClassLoader().getResource("config.properties").getPath(), "UTF-8");
+            File file = new File(propFileName);
+            FileInputStream fis = new FileInputStream(file.getAbsoluteFile());
+            prop.load(fis);
+            if (fis == null)
+            {
+                throw new FileNotFoundException("Propiedades no encontradas dentro de: '" + propFileName + "'");
+            }
+            //Obtener Valores de la Conexion
+            this.url = prop.getProperty("url");
+            this.user = prop.getProperty("user");
+            this.password = prop.getProperty("password");
+            return true;
+        }
+       catch(Exception ex)
+       {
+           System.out.println(ex.getMessage());
+           return true;
+       }
+    }
 }
